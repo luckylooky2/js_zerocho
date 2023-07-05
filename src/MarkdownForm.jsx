75e62c023a6bc19d37a2c79ed64f4af0ecdcd278
Chanhyung Lee <luckylooky2@naver.com>
2023-07-05 15:15:46 +0900

    Chore : react-markdown install, setting

---
+++ src/MarkdownForm.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { okaidia } from 'react-syntax-highlighter/dist/esm/styles/prism';
+
+const MarkdownForm = ({ content }) => {
+  return (
+    <ReactMarkdown
+      children={content}
+      components={{
+        code({ node, inline, className, children, ...props }) {
+          const match = /language-(\w+)/.exec(className || '');
+          return !inline && match ? (
+            <SyntaxHighlighter
+              {...props}
+              children={String(children).replace(/\n$/, '')}
+              style={okaidia}
+              language={match[1]}
+              PreTag="div"
+            />
+          ) : (
+            <code {...props} className={className}>
+              {children}
+            </code>
+          );
+        },
+      }}
+    />
+  );
+};
+
+export default MarkdownForm;
